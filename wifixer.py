7a911b92f5c1352e5ffdfceedbf8c5898acc5d99
Riswanda N.S <root.devilscream@gmail.com>
2016-05-10 23:58:20 +0800

    Add def check_file, check_os, and application

--- wifixer.py
+++ wifixer.py
@@ -5,10 +5,21 @@ import sys
 
 if not os.geteuid() == 0:
     sys.exit('Script must be run as root')
+    
+def check_file(name):
+	return os.path.exists(name)
+	
+def check_os():
+	if check_file("/etc/arch-release") == True:
+		return "arch"
+	elif check_file("/etc/lsb-release") == True:
+		return "ubuntu"
+	else:
+		return 0
 
 def install(driver):
 	os.system("sudo modprobe -r " + driver)
-	if os.path.exists("/etc/modprobe.d/" + driver + ".conf") == True:
+	if check_file("/etc/modprobe.d/" + driver + ".conf") == True:
 		os.remove("/etc/modprobe.d/" + driver + ".conf")
 	file = open("/etc/modprobe.d/" + driver + ".conf", "w")
 	file.write("options " + driver + " fwlps=N ips=N")
@@ -18,6 +29,15 @@ def install(driver):
 def notify(driver):
 	os.system('notify-send "WiFi Connection" "Success Installed driver ' + driver + '" -i notification-network-wireless-connected')
 
+def application(driver, validation):
+	if validation == "y":
+		install(driver)
+		if check_os() == "ubuntu":
+			notify(driver)
+		print "Reboot your computer"
+	else:
+		exit
+	
 color = '\033[91m'
 default = '\033[92m'
 print default + """\
@@ -31,17 +51,11 @@ print default + """\
                                                   
                                                   
 """
-if os.path.exists("/etc/arch-release") != True:
+if check_os() == "ubuntu":
 	os.system("TYPE=$(lspci -nnk | grep -A3 0280 | grep driver); echo $TYPE")
-else:
-	os.system("lspci")
+elif check_os() == "arch":
+	os.system("lspci | grep 'Network'")
 
 driver = raw_input(color + "Wifi driver name: " + default)
 validation = raw_input(color + "Are you sure want to fix your wifi? (y/n) " + default)
-if validation == "y":
-	install(driver)
-	if os.path.exists("/etc/arch-release") != True:
-		notify(driver)
-	print "Reboot your computer"
-else:
-	exit
+application(driver,validation)
